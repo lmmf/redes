b82402aa6176482056c72c6a9656841bc7a1d750
Leonardo <leonardo.mmferreira@gmail.com>
2016-04-15 23:39:31 -0300

    TESTE

--- provinha_2_MIME.py
+++ provinha_2_MIME.py
@@ -11,8 +11,8 @@ app.config.update(dict(
 	#EMAIL SETTINGS
 	MAIL_SERVER='smtp.googlemail.com',
 	MAIL_PORT=587,
-	MAIL_USE_SSL=True,
-	MAIL_USE_TLS=False,
+	MAIL_USE_SSL=False,
+	MAIL_USE_TLS=True,
 	MAIL_USERNAME = 'freddysampaio9@gmail.com',
 	MAIL_PASSWORD = 'fantauva'
 	))
